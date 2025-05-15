c7622c0c9389f6a3ef97015cb629d375453bf9e3
JayTongue <jctung2@illinois.edu>
2025-05-15 09:03:16 -0500

    Actually finished 10

--- 10.py
+++ 10.py
@@ -4,6 +4,8 @@ for i in range(3, int(2e6)):
     for prime in primes:
         if i % prime == 0:
             flag = False
+        if prime > i ** (1/2):
+            break
     if flag:
         primes.append(i)
 print(sum(primes))
