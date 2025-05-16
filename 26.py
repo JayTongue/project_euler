ecd8f9761ab99b33ad307d597797ea05f119b1b6
JayTongue <jaytongue@gmail.com>
2025-05-16 17:02:46 -0500

    started 26

---
+++ 26.py
@@ -0,0 +1,21 @@
+def recurring(numer, denom):
+    print(numer/denom)
+    remainders = {}
+    remainder = numer % denom
+    position = 0
+
+    while remainder and remainder not in remainders:
+        remainders[remainder] = position
+        remainder = (remainder * 10) % denom
+        position += 1
+    print(remainders)
+    if remainder == 0:
+        return 0
+    else:
+        return position - remainders[remainder]
+
+maxi, answer  = 0, 0
+for i in range(1000):
+    if recurring(1, i) > maxi:
+        maxi = i
+print()
