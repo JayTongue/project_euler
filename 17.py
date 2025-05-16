7ee3384d762912ade4057b7aa6e5213ba5f8cdb5
JayTongue <jaytongue@gmail.com>
2025-05-16 10:51:03 -0500

    finsihed 17, but with library cheat

--- 17.py
+++ 17.py
@@ -1,16 +1,3 @@
 from num2words import num2words
 import re
 print(sum(len(re.sub(r'-| ', '', num2words(i, to='cardinal'))) for i in range(1, 1001)))
-
-# for i in range(1, 1001):
-#     print(re.sub(r'-| ', '', num2words(i, to='cardinal')))
-print(num2words(36))
-
-# Other variants, according to the type of article.
-print(num2words(36, to = 'ordinal'))
-print(num2words(36, to = 'ordinal_num'))
-print(num2words(36, to = 'year'))
-print(num2words(36, to = 'currency'))
-
-# Language Support.
-print(num2words(36, lang ='es'))
