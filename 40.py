5239777cc439a8370cf5c19c97dc009b58e63f78
JayTongue <jctung2@illinois.edu>
2025-06-12 16:56:52 -0500

    much faster version of 40

--- 40.py
+++ 40.py
@@ -1,13 +1,11 @@
 from tqdm import tqdm
-
-big_str = ''
+big_list = []
 for i in tqdm(range(1, int(1e6)+1)):
-    big_str += str(i)
-big_str = list(big_str)
-print(int(big_str[0]) 
-      * int(big_str[int(1e1)-1]) 
-      * int(big_str[int(1e2)-1]) 
-      * int(big_str[int(1e3)-1]) 
-      * int(big_str[int(1e4)-1]) 
-      * int(big_str[int(1e5)-1]) 
-      * int(big_str[int(1e6)-1]))
+    big_list += [int(n) for n in list(str(i))]
+print(big_list[0] 
+      * big_list[int(1e1)-1] 
+      * big_list[int(1e2)-1] 
+      * big_list[int(1e3)-1] 
+      * big_list[int(1e4)-1] 
+      * big_list[int(1e5)-1] 
+      * big_list[int(1e6)-1])
