0dff5a387c4260ec6bee401735c2fccc98d5e10a
JayTongue <jaytongue@gmail.com>
2025-06-13 10:40:11 -0500

    finished 44.py

--- 44.py
+++ 44.py
@@ -7,3 +7,4 @@ for i in range(1, 10000):
     for j in range(1, 10000):
         if (pentagon(i) + pentagon(j) in precompute) and (abs(pentagon(i) - pentagon(j)) in precompute):
             print(i, j, abs(pentagon(i) - pentagon(j)))
+            
