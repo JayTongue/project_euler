7a4b183176b65453f5f1fdf90f5e68794dd10193
JayTongue <jctung2@illinois.edu>
2025-07-10 08:35:50 -0500

    Finished 59. Tricky, but comprehensible

--- 59.py
+++ 59.py
@@ -1,16 +1,16 @@
 from itertools import permutations
-from tqdm import tqdm
 
 def str_xor(s1, s2):
-#  return s1 ^ s2
- return "".join([chr(ord(c1) ^ ord(c2)) for (c1,c2) in zip(s1,s2)])
+ return chr(ord(s1) ^ ord(s2))
 
 with open('data/58.txt', 'r') as data:
-   data = data.read().split(',')
+   data = list(map(chr, map(int, data.read().split(','))))
+   # print(data)
 
-for lowercase in tqdm(set(permutations(list('abcdefghijklmnopqrstuvwxyz')*3, 3))):
+for lowercase in set(permutations(list('abcdefghijklmnopqrstuvwxyz')*3, 3)):
     lowercase_exp = lowercase*(len(data)//3)
     result = ''.join([str_xor(i, j) for i, j in zip(data, lowercase_exp)])
-    if "to" in result.lower() and "^" not in result.lower() and "@" not in result:
+    if not any(char in result for char in ['*', '^', '@', '%', '$', '{']):
        print(lowercase, result)
-       print('-------------')
+       break
+print(sum(map(ord, [i for i in list(result)])))
