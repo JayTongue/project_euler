d7b2fceaec16acfe3d5815fce156d54d1f944ffe
JayTongue <jctung2@illinois.edu>
2025-07-14 17:05:39 -0500

    update timer func

--- helpers/timer.py
+++ helpers/timer.py
@@ -4,6 +4,13 @@ def timer(func):
         t1 = time()
         result = func(*args, **kwargs)
         t2 = time()
-        print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s')
+        diff = float(t2-t1)
+        if diff < 60:
+            message = 'Great!'
+        if 60 < diff < 300:
+            message = 'Needs work...'
+        elif diff > 300:
+            message = 'You solution is bad and you should feel bad.'
+        print(f'-------- | Function {func.__name__!r} executed in {diff:.4f}s. | {message} | ------------')
         return result
     return wrap_func
