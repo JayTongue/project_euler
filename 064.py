7b1ca816596efa4b476f0140245cd45b9c11c28a
JayTongue <jctung2@illinois.edu>
2025-07-17 16:28:23 -0500

    added Decimal precison, but not enough

--- 064.py
+++ 064.py
@@ -1,4 +1,10 @@
-from math import sqrt
+from fractions import Fraction
+from math import isqrt, sqrt
+from decimal import Decimal, getcontext
+from helpers.timer import timer
+from tqdm import tqdm
+
+getcontext().prec = 1000
 
 def find_segments(loop):
     for i in range(0, len(loop)):
@@ -7,22 +13,51 @@ def find_segments(loop):
         if repeated == loop[:len(repeated)]:
             return chunk
         
+# def continued_fraction_period(n):
+#     m, d, a0 = 0, 1, isqrt(n)
+#     if a0 * a0 == n:
+#         return 0  # perfect square, no period
 
+#     a = a0
+#     period = 0
+#     seen = set()
+
+#     while True:
+#         m = d * a - m
+#         d = (n - m * m) // d
+#         a = (a0 + m) // d
+#         period += 1
+#         if a == 2 * a0:
+#             return period
+        
 def continued_fractions(start):
+    root = start.sqrt()
     if sqrt(start) % 1 == 0:
         return []
-    a0 = int(sqrt(start) // 1)
-    a1 = 1/(sqrt(start) - a0)
+    a0 = int(root)
+    a1 = Decimal(1) / (root - a0)
     loops = [int(a1)]
-    for _ in range(18):
-        a2 = 1 / (a1 - int(a1))
+    for _ in range(150):
+        a2 = Decimal(1) / (a1 - int(a1))
         loops.append(int(a2))
         a1 = a2
     chunk = find_segments(loops)
     return chunk
 
-search_space = 13
-for i in range(2, search_space + 1):
-    chunk = continued_fractions(i)
-    print(chunk)
-    
+@timer
+def main():
+    search_space = int(1e4)
+    sol = 0
+
+    # for n in tqdm(range(2, limit + 1)):
+    #     if continued_fraction_period(n) % 2 == 1:
+    #         sol += 1
+
+    for i in tqdm(range(2, search_space + 1)):
+        chunk = continued_fractions(i)
+        if chunk:
+            if len(chunk) % 2 == 1:
+                sol += 1
+    print(sol)
+
+main()
