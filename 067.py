880f3f669d6cab121ad4fd8c95cae2648cd1f4c8
JayTongue <jaytongue@gmail.com>
2025-08-06 14:35:46 -0500

    super elegent and fast version of 67

--- 067.py
+++ 067.py
@@ -1,28 +1,8 @@
-'''
-given 
-
-      3
-     7 4
-    2 4 6
-   8 5 9 3
-
-
-Start at row: [8, 5, 9, 3]
-Then row above: [2, 4, 6] becomes:
-
-[2 + max(8,5) = 10,
- 4 + max(5,9) = 13,
- 6 + max(9,3) = 15] → [10, 13, 15]
-
-Then: [7,4] → [7 + max(10,13) = 20, 4 + max(13,15) = 19] → [20,19]
-
-Finally: [3 + max(20,19) = 23]
-
-'''
-
 with open('data/67.txt', 'r') as data:
-    data = data.readlines()[:-2]
+    data = data.readlines()
     data = [list(map(int, line.split(' '))) for line in data]
 
-for count, number in enumerate(data[-2]):
-    
+new_bottom = [i + (max(data[-1][count], data[-1][count+1])) for count, i in enumerate(data[-2])]
+for line_counter in range(3, len(data)+1):
+    new_bottom = [i + max(new_bottom[count], new_bottom[count+1]) for count, i in enumerate(data[-line_counter])]
+print(new_bottom[0])
