6ea4ce0c79c5eab6d3cfc890bec763119ed677fc
JayTongue <jaytongue@gmail.com>
2025-09-12 08:51:43 -0500

    faster 76

--- 076.py
+++ 076.py
@@ -1,21 +1,15 @@
 from helpers.timer import timer
 
-def find_ways(breakdown, i, n, sol):
-    if n == 0:
-        sol += 1
-        # print(breakdown)
-    else:
-        for j in range(i, n+1):
-            breakdown.append(j)
-            sol = find_ways(breakdown, j, n-j, sol)
-            del breakdown[-1]
-    return sol
-
 @timer
 def main():
     n = 100
-    breakdown, sol = [], 0
-    sol = find_ways(breakdown, 1, n, sol)
-    print(sol-1)
+    dp = [0] * (n + 1)
+    dp[0] = 1
+
+    for i in range(1, n + 1):
+        for j in range(i, n + 1):
+            dp[j] += dp[j - i]
+
+    print(dp[n] - 1)
 
 main()
